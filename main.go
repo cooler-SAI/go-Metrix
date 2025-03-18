c7cc57b9028d62b424768bda6b94a40294096782
cooler-SAI <coolerme86@gmail.com>
2025-03-18 20:52:59 +0300

    simple code added

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+
+	for {
+		log.Info().Msg("Hi All Here")
+		time.Sleep(1 * time.Second)
+	}
 }
