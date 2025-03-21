f31f167a9d7a8a2240b87e7e2d0dc0f7437c61bd
cooler-SAI <coolerme86@gmail.com>
2025-03-21 22:06:14 +0300

    added prometheus code in project

--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"net/http"
 	"os"
 	"os/signal"
 	"sync"
 	"syscall"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+)
+
+var (
+	messageCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "message_count",
+		Help: "Total number of messages sent.",
+	})
+	_ = time.Now()
 )
 
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+
+	prometheus.MustRegister(messageCounter)
+
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		if err := http.ListenAndServe(":2112", nil); err != nil {
+			log.Fatal().Err(err).Msg("Failed to start metrics server")
+		}
+	}()
 
 	var wg sync.WaitGroup
 	stopChan := make(chan os.Signal, 1)
@@ -29,6 +49,7 @@ func main() {
 				return
 			default:
 				log.Info().Msg("Hi All Here")
+				messageCounter.Inc()
 				time.Sleep(1 * time.Second)
 			}
 		}
